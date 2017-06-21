d4404e796629bf6a66fe00f64c8fe255fb2422aa
nozawa_soushi <nozawa_soushi@cyberagent.co.jp>
2017-06-21 17:42:04 +0900

    UPDATE setup

--- setup.sh
+++ setup.sh
@@ -15,4 +15,4 @@ fi
 sed -i.org  -e "s/{FCM_SERVER_KEY}/$2/" ./docker-compose.yml
 rm ./docker-compose.yml.org
 sed -i.org  -e "s/{FCM_SENDER_ID}/$1/" ./web/app/js/firebase/init.js
-rm ./web/app/js/firebase/init.js
+rm ./web/app/js/firebase/init.js.org
